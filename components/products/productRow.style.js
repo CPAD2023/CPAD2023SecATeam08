5cecc18b07ce257fa2ed013101be772bfc194f31
SrijaniChakroborty <128456915+SrijaniChakroborty@users.noreply.github.com>
2023-11-30 23:38:27 +0530

    Delete components directory

--- components/products/productRow.style.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from "react-native";
-import { COLORS,SIZES } from "../../constants";
-
-const styles=StyleSheet.create({
-    container:{marginTop:SIZES.medium, marginLeft: 12}
-
-})
-export default styles
