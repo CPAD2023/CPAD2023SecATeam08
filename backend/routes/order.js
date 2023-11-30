2605a2652464639ddad724f96a4465ac5875f2b8
SrijaniChakroborty <128456915+SrijaniChakroborty@users.noreply.github.com>
2023-11-30 23:38:18 +0530

    Delete backend directory

--- backend/routes/order.js
+++
@@ -1,7 +0,0 @@
-const router=require('express').Router();
-const ordersController=require('../controllers/ordersController')
-
-router.get('/:id',ordersController.getUserOrders)
-
-
-module.exports=router;
--- backend/routes/user.js
+++
@@ -1,7 +0,0 @@
-const router=require('express').Router();
-const userController=require('../controllers/userController');
-
-router.delete('/:id',userController.deleteUser);
-router.get('/:id',userController.getUser);
-
-module.exports=router;
